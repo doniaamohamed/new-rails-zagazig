9f6b58d5eb7478dd55e8d2733acd63cb6c2a5394
donia <doniaamohamed498@gmail.com>
2026-06-08 05:17:40 +0300

    add post model with associations

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
+  has_many :post_editors
+  has_many :editors, through: :post_editors
+
+
+end
